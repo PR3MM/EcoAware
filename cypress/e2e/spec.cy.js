6278bdfd8982fdd107a7fe244946dbd82731f803
PR3MM <premveddhote@gmail.com>
2026-01-07 22:17:56 +0530

    test cases

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,34 @@
+describe('first test', () => {
+  it('visit the page', () => {
+    cy.visit('http://localhost:5173')
+    // cy.get("[data-cy='navbar-home']").should('be.visible')
+    // cy.get("[data-cy='navbar-collection']").should('be.visible')
+    cy.contains('Collection').click()
+    // cy.contains('Guide').click()
+    // cy.contains('Quiz').click()
+    // cy.contains('Home').click()
+
+    cy.contains('Schedule E-Waste Collection')
+    cy.get('[name="name"]')
+    cy.get('[name="email"]')
+    cy.get('[name="address"]')
+    cy.get('[name="type"]')
+    cy.get('[name="weight"]')
+    cy.get(':nth-child(5) > .w-full')
+    
+    // should not submit
+    // cy.get(':nth-child(5) > .w-full').click().should('have.value',false)
+
+    // enter text 
+
+    cy.get('[name="name"]').type("sda")
+    cy.get('[name="email"]').type("sda@a.com")
+    cy.get('[name="address"]').type("sda")
+    cy.get('[name="type"]').select("Battery")
+    cy.get('[name="weight"]').type(1)
+    cy.get(':nth-child(5) > .w-full').click()
+
+    cy.get('.mt-4').should('be.visible')
+
+  })
+})
